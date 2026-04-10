8d8c2836f76d9c5ca5a2debccebce6ddab4ca661
Ritik23-bit <ritik.agarwal672@gmail.com>
2026-04-10 11:09:46 +0000

    size updation

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "ritk@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 4 // not allowed
+
+accountEmail = "rk@yahoo.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+/*
+Prefer not to use var
+as issue in block & functional scope
+*/
